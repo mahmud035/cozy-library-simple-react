4b1deb12c6ad4959ba7e9993b3b81a5d80d62a25
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-26 03:18:40 +0600

    add to cart button added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
@@ -11,6 +13,10 @@ const Product = (props) => {
         <h2>{name}</h2>
         <h4>Price: {price}</h4>
       </div>
+      <button className="btn-cart">
+        <p className="btn-text">Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
